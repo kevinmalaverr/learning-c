885b6000357a6b1820b015cca74776c8daf4536a
kevinmalaverr <kevinmalaverr@gmail.com>
2020-12-27 17:05:49 -0500

    cpp hello world

---
+++ cpp/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<10<<endl;
+    cout<<3.1416f<<endl;
+    cout<<true<<endl;
+    cout<<'c'<<endl;
+
+    cout<<"hello world"<<endl;
+    return 0;
+}
